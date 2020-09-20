a4bcb610c58c199da1be43aca937b087e8ce3993
Rob Martin <rmartin3@alumni.unca.edu>
2020-09-19 18:02:49 -0700

    home container setup with pokemon actions and reducers

--- src/pokemon.list/index.js
+++ src/pokemon.list/index.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetch_my_pokemon, fetch_pokemon_by_id } from "../app/ducks/pokemon";
 import Button from "@material-ui/core/Button";
 
-const PokemonList = ({ history }) => {
+const PokemonList = ({
+  history,
+  fetch_my_pokemon,
+  my_pokemon,
+  fetch_pokemon_by_id,
+  selected,
+}) => {
+  useEffect(() => {
+    fetch_my_pokemon();
+  }, []);
+
   return (
     <div>
-      pokemon list
+      {my_pokemon.map((pokemon) => (
+        <p>{pokemon.name}</p>
+      ))}
       <Button
         variant="contained"
         color="primary"
@@ -16,4 +30,16 @@ const PokemonList = ({ history }) => {
   );
 };
 
-export default PokemonList;
+const mapStateToProps = (state) => {
+  return {
+    my_pokemon: state.pokemon.my_pokemon,
+    selected: state.pokemon.selected,
+  };
+};
+
+const mapDispatchToProps = {
+  fetch_my_pokemon,
+  fetch_pokemon_by_id,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonList);
