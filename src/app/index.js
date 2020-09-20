a19416f98ed5733538d52b895d06aae2195d7ca3
Rob Martin <rmartin3@alumni.unca.edu>
2020-09-19 17:47:18 -0700

    pokemon actions and reduxers for redux

--- src/app/index.js
+++ src/app/index.js
@@ -4,16 +4,21 @@ import { Canvas } from "./common";
 import PokemonList from "../pokemon.list";
 import Catch from "../catch";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <Canvas>
-      <Router>
-        <Switch>
-          <Route exact path="/pokedex" component={PokemonList} />
-          <Route path="/pokedex/catch" component={Catch} />
-        </Switch>
-      </Router>
-    </Canvas>
+    <Provider store={store}>
+      <Canvas>
+        <Router>
+          <Switch>
+            <Route exact path="/pokedex" component={PokemonList} />
+            <Route path="/pokedex/catch" component={Catch} />
+          </Switch>
+        </Router>
+      </Canvas>
+    </Provider>
   );
 }
 
--- src/catch/index.js
+++ src/catch/index.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import {
+  fetch_wild_pokemon,
+  fetch_pokemon_by_id,
+  deselect_pokemon,
+  catch_pokemon,
+  clear_wild_pokemon,
+} from "../app/ducks/pokemon";
 
-const Catch = () => {
-  return <div>catch</div>;
+const Catch = ({
+  fetch_wild_pokemon,
+  wild_pokemon,
+  fetch_pokemon_by_id,
+  deselect_pokemon,
+  catch_pokemon,
+  clear_wild_pokemon,
+  selected,
+}) => {
+  useEffect(() => {
+    fetch_wild_pokemon();
+
+    return clear_wild_pokemon();
+  }, []);
+  return (
+    <div>
+      {wild_pokemon.map((pokemon) => (
+        <p key={pokemon.url} onClick={() => fetch_pokemon_by_id(pokemon.url)}>
+          {pokemon.name}
+        </p>
+      ))}
+      <hr />
+      <p
+        // onClick={() => deselect_pokemon()}
+        onClick={() => catch_pokemon(selected)}
+      >
+        {selected?.name}
+      </p>
+    </div>
+  );
 };
 
-export default Catch;
+const mapStateToProps = (state) => {
+  return {
+    wild_pokemon: state.pokemon.wild_pokemon,
+    selected: state.pokemon.selected,
+    is_loading: state.pokemon.is_loading,
+  };
+};
+
+const mapDispatchToProps = {
+  fetch_wild_pokemon,
+  fetch_pokemon_by_id,
+  deselect_pokemon,
+  catch_pokemon,
+  clear_wild_pokemon,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Catch);
